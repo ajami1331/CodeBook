91c82eaa6934b4aff4c5ea44e7854da2bc4a2013
Araf Al-Jami <clown1331@yahoo.com>
2017-01-21 01:08:23 +0600

    Updated

--- Graph/EulerianPath.cpp
+++ Graph/EulerianPath.cpp
@@ -5,19 +5,19 @@
     Input:
         - adj, graph
     Tested Problems:
-      CodeChef:
-        TOURISTS - Tourists in Mancunia
+        CodeChef:
+            TOURISTS - Tourists in Mancunia
 **/
 struct Edge;
 typedef list< Edge >::iterator iter;
 
 struct Edge {
-	int next_vertex;
-	iter reverse_edge;
+    int next_vertex;
+    iter reverse_edge;
 
-	Edge(int next_vertex)
-		:next_vertex(next_vertex)
-		{ }
+    Edge( int next_vertex )
+        :next_vertex(next_vertex)
+        { }
 };
 
 const int sz = 1e5 + 10;
@@ -27,20 +27,20 @@ list < Edge > adj[ max_vertices ];
 vector< int > path;
 
 void find_path( int v ) {
-	while( adj[v].size() > 0 ) {
-		int vn = adj[v].front().next_vertex;
-		adj[vn].erase( adj[v].front().reverse_edge );
-		adj[v].pop_front();
-		find_path( vn );
-	}
-	path.push_back( v );
+    while( adj[v].size() > 0 ) {
+        int vn = adj[v].front().next_vertex;
+        adj[vn].erase( adj[v].front().reverse_edge );
+        adj[v].pop_front();
+        find_path( vn );
+    }
+    path.push_back( v );
 }
 
 void add_edge( int a, int b ) {
-	adj[ a ].push_front( Edge( b ) );
-	iter ita = adj[ a ].begin();
-	adj[ b ].push_front( Edge( a ) );
-	iter itb = adj[ b ].begin();
-	ita->reverse_edge = itb;
-	itb->reverse_edge = ita;
+    adj[ a ].push_front( Edge( b ) );
+    iter ita = adj[ a ].begin();
+    adj[ b ].push_front( Edge( a ) );
+    iter itb = adj[ b ].begin();
+    ita->reverse_edge = itb;
+    itb->reverse_edge = ita;
 }
