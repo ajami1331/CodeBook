fea591c9a36e12c65c84587a6256e75b5957ecd1
CLown1331 <clown1331@yahoo.com>
2017-09-22 22:06:30 +0600

    Fraction

--- Data Structure/Fraction.cpp
+++ Data Structure/Fraction.cpp
@@ -14,4 +14,36 @@ struct fraction {
         thik_kor();
         return *this;
     }
+    fraction operator -= ( const fraction &rhs ) {
+        gcd = __gcd( down, rhs.down );
+        lcm = ( down / gcd ) * rhs.down;
+        up = ( ( lcm / down ) * up ) - ( ( lcm / rhs.down ) * rhs.up );
+        down = lcm;
+        thik_kor();
+        return *this;
+    }
+    fraction operator *= ( const fraction &rhs ) {
+        gcd = __gcd( up, down );
+        up /= gcd;
+        down /= gcd;
+        gcd = __gcd( rhs.up, rhs.down );
+        up =  up * ( rhs.up / gcd );
+        down =  down * ( rhs.down / gcd );
+        thik_kor();
+        return *this;
+    }
+    bool operator < ( const fraction &rhs ) const {
+        ll gc =  __gcd( down, rhs.down );
+        ll lc = ( rhs.up / gc ) * up;
+        ll a = ( lc / down ) * up;
+        ll b = ( lc / rhs.down ) * rhs.up;
+        return a < b;
+    }
+    bool operator > ( const fraction &rhs ) const {
+        ll gc =  __gcd( down, rhs.down );
+        ll lc = ( rhs.up / gc ) * up;
+        ll a = ( lc / down ) * up;
+        ll b = ( lc / rhs.down ) * rhs.up;
+        return a > b;
+    }
 } ans, inp;
