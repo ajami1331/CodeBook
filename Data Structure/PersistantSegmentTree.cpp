0e3d99ff31c7c29fcbc3c197165d880ee7f2d71f
CLown1331 <clown1331@yahoo.com>
2017-10-03 23:40:14 +0600

    Static Persistant Segment tree

--- Data Structure/PersistantSegmentTree.cpp
+++ Data Structure/PersistantSegmentTree.cpp
@@ -1,65 +1,63 @@
 const int sz = 1e5 + 10;
 int ar[sz];
-
 struct node {
-    node *left;
-    node *right;
+    int left;
+    int right;
     int val;
+} nodes[sz * 32];
+int root[sz];
 
-    node( int val = 0, node *left = nullptr, node *right = nullptr ) {
-        this->val = val;
-        this->left = left;
-        this->right = right;
+void build( int nd, int l, int r ) {
+    if( l == r ) {
+        nodes[nd].val = 0;
+        return;
     }
+    int mid = ( l + r ) >> 1;
+    nodes[nd].left = ++nxt;
+    nodes[nd].right = ++nxt;
+    build( nodes[nd].left, l, mid );
+    build( nodes[nd].right, mid + 1, r );
+    nodes[nd].val = nodes[ nodes[nd].left ].val + nodes[ nodes[nd].right ].val;
+}
 
-    void build( int l, int r ) {
-        if( l == r ) {
-            this->val = ar[l];
-            return;
-        }
-        int mid = ( l + r ) >> 1;
-        this->left = new node();
-        this->right = new node();
-        this->left->build( l, mid );
-        this->right->build( mid + 1, r );
-        this->val = this->left->val + this->right->val;
+int update( int nd, int l, int r, int idx, int x ) {
+    if( r < idx || idx < l ) {
+        return nd;
     }
+    if( l == r ) {
+        int n_nd = ++nxt;
+        nodes[ n_nd ].val = nodes[ nd ].val + x;
+        nodes[ n_nd ].left = nodes[ nd ].left;
+        nodes[ n_nd ].right = nodes[ nd ].right;
+        return n_nd;
+    }
+    int mid = ( l + r ) >> 1;
+    int n_nd = ++nxt;
+    nodes[ n_nd ].val = nodes[ nd ].val;
+    nodes[ n_nd ].left = update( nodes[ nd ].left, l, mid, idx, x );
+    nodes[ n_nd ].right = update( nodes[ nd ].right, mid + 1, r, idx, x );
+    nodes[ n_nd ].val = nodes[ nodes[ n_nd ].left ].val + nodes[ nodes[ n_nd ].right ].val;
+    return n_nd;
+}
 
-    node *update( int l, int r, int idx, int x ) {
-        if( r < idx || idx < l ) {
-            return this;
-        }
-        if( l == r ) {
-            node *ret = new node( this->val, this->left, this->right );
-            ret->val += x;
-            return ret;
-        }
-        int mid = ( l + r ) >> 1;
-        node *ret = new node( this->val );
-        ret->left = this->left->update( l, mid, idx, x );
-        ret->right = this->right->update( mid + 1, r, idx, x );
-        ret->val = ret->left->val + ret->right->val;
-        return ret;
+int query( int nd, int l, int r, int i, int j ) {
+    if( r < i || l > j ) {
+        return 0;
     }
-
-    int query( int l, int r, int i, int j ) {
-        if( r < i || l > j ) {
-            return 0;
-        }
-        if( i <= l && r <= j ) {
-            return this->val;
-        }
-        int mid = ( l + r ) >> 1;
-        return this->left->query( l, mid, i, j ) + this->right->query( mid + 1, r, i, j );
+    if( i <= l && r <= j ) {
+        return nodes[ nd ].val;
     }
-} *root[sz];
+    int mid = ( l + r ) >> 1;
+    return query( nodes[ nd ].left, l, mid, i, j ) + query( nodes[ nd ].right, mid + 1, r, i, j );
+}
 
 int main() {
     ar[] = { 1, 2, 3 };
-    root[0] = new node();
-    root[0]->build( 0, 2 );
-    root[1] = root[0]->update( 0, 2, 1, 1 );
-    root[1] = root[1]->update( 0, 2, 1, 1 );
-    printf( "%d\n", root[0].query( 0, 2, 0, 2 ) );
-    printf( "%d\n", root[1].query( 0, 2, 0, 2 ) );
+    nxt = -1;
+    root[0] = ++nxt;
+    build( root[0], 0, 2 );
+    root[1] = update( root[0], 0, 2, 1, 1 );
+    root[1] = update( root[1], 0, 2, 1, 1 );
+    printf( "%d\n", query( root[0], 0, 2, 0, 2 ) );
+    printf( "%d\n", query( root[1], 0, 2, 0, 2 ) );
 }
