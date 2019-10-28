d6feccf0ec02ad607a353425a96d0572d3b431e1
Araf Al-Jami <CLown1331@users.noreply.github.com>
2019-10-29 02:40:30 +0600

    Update big_mod.cpp

--- Math/big_mod.cpp
+++ Math/big_mod.cpp
@@ -1,12 +1,12 @@
-ll big_mod( ll b, ll p ) {
-        ll ret = 1;
-        for( ; p>0; p >>= 1 ) {
-                if( p&1 ) ret = ( ret * b ) % mod;
-                b = ( b * b ) % mod;
-        }
-        return ret % mod;
+ll mod_pow(ll b, ll p) {
+    ll ret = 1;
+    for( ; p > 0; p >>= 1 ) {
+        if( p&1 ) ret = ( ret * b ) % mod;
+        b = ( b * b ) % mod;
+    }
+    return ret % mod;
 }
 
 ll mod_inv( ll b ) {
-        return big_mod( b, mod - 2 );
+    return mod_pow( b, mod - 2 );
 }
